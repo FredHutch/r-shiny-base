ee72e28c6f3cd8a40b21271fd5f22f1269ecf8b0
Dan Tenenbaum <dtenenba@fhcrc.org>
2023-07-26 09:49:38 -0700

    build version based on R-4.2.2; verify pkg installation

--- installRpackages.R
+++ installRpackages.R
@@ -47,3 +47,21 @@ install.packages("DT")
 install.packages("igraph")
 install.packages("intergraph")
 install.packages("ggnetwork")
+
+###
+
+pkg <- c("shinty", "shinyBS", "shinythemes", "readxl", "tidyverse", "mi", "descr", "pROC", "readxl", "plyr", "tidyverse", "assertthat", "ggplot2", "cowplot", "viridis", "ggthemes", "car", "ggExtra", "scales", "xtable", "markdown", "htmltools", "rsconnect", "rmeta", "shiny.semantic", "DT", "dplyr", "plotly", "webshot", "reshape2", "htmlwidgets", "maps", "mapproj", "methods", "bitops", "data.table", "httr", "jsonlite", "REDCapR", "aws.s3", "DT", "igraph", "intergraph", "ggnetwork")
+
+pkgNames <- names(installed.packages()[, "Package"])
+
+installed <- pkg %in% pkgNames
+
+nres <- cbind(pkg, installed)
+
+print(nres)
+
+if (any(!installed)) {
+    q("no", 1)
+}
+
+
